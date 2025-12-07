d7778dc4fa41d565e1b978ad18b7bff0b1bde610
Nur Hossain <nur1952@gmail.com>
2025-12-07 20:05:39 +0600

    1.5.8 realese

--- EduPress.php
+++ EduPress.php
@@ -6,7 +6,7 @@ Plugin Name: EduPress
 Plugin URI: https://edupressbd.com/
 Description: School Management Software
 Author: Mohammad Nur Hossain
-Version: 1.5.6
+Version: 1.5.8
 Author URI: https://nur.codist.dev/
 Text Domain: edupress
 Domain Path: /languages
